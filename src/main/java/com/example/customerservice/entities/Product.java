b58f8898d467b83b4ef5f9696df1dd2cefb2bd1b
asmae.bouchekara@gmail.com <asmaebouch>
2023-11-26 14:26:59 -0200

    second commit

--- src/main/java/com/example/customerservice/entities/Product.java
+++ src/main/java/com/example/customerservice/entities/Product.java
@@ -10,14 +10,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Data@AllArgsConstructor@NoArgsConstructor
 @Builder
-public class Customer {
-    @Id @GeneratedValue (strategy = GenerationType.IDENTITY)
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private String email;
-    private Long productId;
+    private double price;
 }
--- src/main/java/com/example/customerservice/repository/CustomerRepositor.java
+++ src/main/java/com/example/customerservice/repository/CustomerRepositor.java
@@ -4,7 +4,6 @@ import com.example.customerservice.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
 public interface CustomerRepositor extends JpaRepository <Customer,Long>{
 
 }
